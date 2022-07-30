bda7e03e1319188f52e4310dedfb78210a175fce
KU13 <41525944+KU13-07@users.noreply.github.com>
2022-07-30 10:06:07 -0400

    0.2
    - Added basic image functionality (WIP)

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import discord
+from PIL import Image, ImageDraw
+from io import BytesIO
+import requests
 
 from config import *
 
@@ -8,8 +11,30 @@ bot = discord.Bot()
 async def on_ready():
     print("Ready")
 
-@bot.slash_command(guild_ids=GUILD_IDS)
-async def test(ctx):
-    await ctx.respond('hi')
+@bot.slash_command(guild_ids=GUILD_IDS, description='h')
+async def test(ctx, url: discord.Option(str, "url")):
+    try:
+        response = requests.get(url)
+    except:
+        response = requests.get(str(ctx.author.display_avatar))
+        print(str(ctx.author.display_avatar))
+
+    img = Image.new('RGB', (1570,942), color='grey')
+
+    
+    img2 = Image.open(BytesIO(response.content))
+
+    mask_img = Image.new("L", img2.size, 0)
+    draw = ImageDraw.Draw(mask_img)
+    draw.ellipse((140,50,260,170), fill=255)
+
+    img2.reduce
+
+
+    img.paste(img2, (157,157), mask_img)
+
+
+    img.save('test.png')
+    await ctx.respond(file=discord.File('test.png'))
 
 bot.run(BOT_TOKEN)
